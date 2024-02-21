75d466fc61e38a7ffdecb8b9fe12503cd64d4e56
Michael Scharf <michael55.scharf@outlook.com>
2024-02-21 09:43:27 -0500

    Start

---
+++ src/test/java/ca/sheridancollege/scharfm/midtermscharfm/MidtermScharfMApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.scharfm.midtermscharfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermScharfMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
